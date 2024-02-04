2789204c9de190ec664336d3e0e099b85379e98d
Wasi <wbhuyian201286@bscse.uiu.ac.bd>
2024-02-04 22:56:37 +0600

    Project Section Done

---
+++ src/app/components/Projects.jsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+import ProjectItem from './ProjectItem'
+
+const Projects = () => {
+  return (
+    <div className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
+        <h2 className='py-4'>My projects</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+            <ProjectItem title="ProjectName" backgroundImg='' projectUrl='' />
+            <ProjectItem title="ProjectName" backgroundImg='' projectUrl='' />
+            <ProjectItem title="ProjectName" backgroundImg='' projectUrl='' />
+            <ProjectItem title="ProjectName" backgroundImg='' projectUrl='' />
+            <ProjectItem title="ProjectName" backgroundImg='' projectUrl='' />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Projects
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import Main from "./components/Main";
 import About from "./components/About";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+// import Contact from "./components/Contact";
 import Head from "next/head";
 export default function Home() {
   return (
@@ -13,6 +15,7 @@ export default function Home() {
     <Main />
     <About />
     <Skills />
+    <Projects />
    </div>
   );
 }
